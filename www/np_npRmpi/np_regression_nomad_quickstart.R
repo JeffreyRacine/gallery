2c32dfee882ea33dc3169684b89c80aa8998a8c0
Jeffrey Racine <racinej@mcmaster.ca>
2026-04-30 06:50:05 -0400

    Simplify LP quickstart example

--- www/np_npRmpi/np_regression_nomad_quickstart.R
+++ www/np_npRmpi/np_regression_nomad_quickstart.R
@@ -1,31 +1,6 @@
-## Minimal np local-polynomial NOMAD example.
-##
-## This is the modern convenience route for supported LP workflows:
-## 1. call the estimator with nomad = TRUE,
-## 2. inspect the normalized shortcut metadata,
-## 3. summarize and plot the fit.
-
-if (!requireNamespace("crs", quietly = TRUE)) {
-  stop(
-    "This quickstart uses nomad = TRUE, which requires the 'crs' package ",
-    "for NOMAD degree search. Install it with install.packages('crs')."
-  )
-}
-
 library(np)
-options(np.messages = FALSE)
+data(cps71, package = "np")
 
-set.seed(7)
-n <- 120
-x <- runif(n, -1, 1)
-y <- x + 0.4 * x^2 + rnorm(n, sd = 0.18)
-dat <- data.frame(y, x)
-
-fit <- npreg(y ~ x, data = dat, nomad = TRUE, degree.max = 2L, nmulti = 1L)
-
-fit$bws$nomad.shortcut
-summary(fit)
-
-plot(dat$x, dat$y, cex = 0.35, col = "grey")
-o <- order(dat$x)
-lines(dat$x[o], fitted(fit)[o], col = 2, lwd = 2)
+fit_lp <- npreg(logwage ~ age, data = cps71, nomad = TRUE)
+plot(fit_lp)
+summary(fit_lp)
