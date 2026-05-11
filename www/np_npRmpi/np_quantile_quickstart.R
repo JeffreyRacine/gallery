0b483532cea51d6e32c85f71bda5aa48ba5a5191
Jeffrey Racine <racinej@mcmaster.ca>
2026-05-11 09:44:37 -0400

    Refresh npqreg quantile workflow

--- www/np_npRmpi/np_quantile_quickstart.R
+++ www/np_npRmpi/np_quantile_quickstart.R
@@ -1,7 +1,7 @@
 ## Minimal np quantile-regression example.
 ##
-## The key idea is to compute a conditional-distribution bandwidth once
-## and then reuse it for more than one quantile.
+## The key idea is to compute/select the conditional distribution once
+## and then extract one or more quantiles from it.
 
 library(np)
 options(np.messages = FALSE)
@@ -10,19 +10,15 @@ options(np.messages = FALSE)
 data(faithful, package = "datasets")
 dat <- faithful[seq_len(120), c("eruptions", "waiting")]
 
-## Build one conditional-distribution bandwidth object and reuse it.
-bw <- npcdistbw(eruptions ~ waiting, data = dat, nmulti = 1)
-q25 <- npqreg(bws = bw, tau = 0.25)
-q50 <- npqreg(bws = bw, tau = 0.50)
-q75 <- npqreg(bws = bw, tau = 0.75)
+## Build one conditional-distribution fit and request several quantiles.
+qfit <- npqreg(eruptions ~ waiting,
+               data = dat,
+               tau = c(0.25, 0.50, 0.75),
+               nmulti = 1)
 
 ## Review the bandwidth choice and one representative fitted quantile.
-summary(bw)
-summary(q50)
+summary(qfit$bws)
+summary(qfit)
 
-## Plot the data once, then overlay the fitted quantile curves.
-plot(dat$waiting, dat$eruptions, cex = 0.35, col = "grey")
-o <- order(dat$waiting)
-lines(dat$waiting[o], q25$quantile[o], col = 2, lty = 2, lwd = 2)
-lines(dat$waiting[o], q50$quantile[o], col = 4, lty = 1, lwd = 2)
-lines(dat$waiting[o], q75$quantile[o], col = 2, lty = 3, lwd = 2)
+## Plot the overlaid fitted quantile curves.
+plot(qfit)
